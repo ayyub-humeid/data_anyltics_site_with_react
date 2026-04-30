468f77e99b5355fcc3a8810dfa14141e99ad3b98
ayyub humeid <ayoubhumeid@gmail.com>
2026-04-30 09:58:45 +0300

    feat: install react-typed and add typing animation to Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ReactTyped } from "react-typed";
 const Hero = () => {
   //   const [word, setWord] = useState("BTB");
 
@@ -11,10 +12,17 @@ const Hero = () => {
         <h1 className="font-bold text-4xl sm:text-6xl md:text-7xl md:py-4 font-bold">
           Grow With Data
         </h1>
-        <div>
-          <p className="text-xl sm:text-4xl md:5xl font-bold py-4">
-            Fast , Flexible financing for{" "}
+        <div className="flex justify-center items-center">
+          <p className="text-xl sm:text-4xl md:text-5xl font-bold py-4">
+            Fast, flexible financing for
           </p>
+          <ReactTyped
+            className="text-xl sm:text-4xl md:text-5xl font-bold md:pl-4 pl-2"
+            strings={["BTB", "BTC", "SASS"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
         </div>
         <p className="text-xl md:text-2xl  text-gray-500 font-bold">
           Monitor your data analytics to incress revenue for BTB, BTC, & SASS
