0e52fe2c6116cc0ff568be30dfdcab1bf47e3629
ayyub humeid <ayoubhumeid@gmail.com>
2026-05-01 21:18:35 +0300

    feat: add service data configuration

---
+++ src/servieces/service.js
@@ -0,0 +1,18 @@
+const services = [
+  {
+    id: 1,
+    title: "Data Analyses",
+    description: "we make descution making more easily . ",
+  },
+  {
+    id: 2,
+    title: "Web Development",
+    description: "we provide comperehencive management system. ",
+  },
+  {
+    id: 3,
+    title: "Graphic design",
+    description: "Travel with our world . ",
+  },
+];
+export default services;
